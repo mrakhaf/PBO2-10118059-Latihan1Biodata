fcd1194c29ed8c6db8301a19d69704b4985dfede
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 16:58:54 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan1biodata/PBO210118059Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan1biodata;
+
+/**
+ *
+ * @author 
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ * 
+ */
+public class PBO210118059Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Muhammad Rakha Firdaus");
+        System.out.println("ALAMAT\t\t: Jalan Semar");
+        System.out.println("TEMPAT LAHIR\t: Tangerang");
+        System.out.println("TANGGAL LAHIR\t : 18 Oktober 2000");
+        System.out.println("---------------------------------");
+        System.out.println("HOBI\t\t: Futsal & travelling");
+        System.out.println("MAKES\t\t: Indomie");
+        System.out.println("MIKES\t\t: Teh Botol");
+    }
+    
+}
